e05c85bbe311ae69e5c8ba2d212730e3f91212b9
wym <wym@sorcerer.co.jp>
2023-04-28 18:32:42 +0900

    useEffectwo

--- src/components/PlayerStatus.tsx
+++ src/components/PlayerStatus.tsx
@@ -1,45 +1,39 @@
 import axios from "axios";
-import {useEffect, useState} from "react";
+import { useState } from "react";
 import { Player } from "../types/player";
 
 const PlayerStatus = () => {
   const dummy: Player[] = [{ id: 1, name: "player1", hp: 100, mp: 100, money: 100 }];
   const [players, setPlayers] = useState<Player[]>([]);
 
-    const fetchData = async () => {
-        const playerId = localStorage.getItem("playerId");
-        try {
-            //自分のapiサーバーにリクエストを送る
-            const res = await axios.get(`http://localhost:3001/players/${playerId}`);
-            const data = await res.data;
-            console.log(data);
-            return data;
-            //TODO 取得したデータをstateに保存
-        } catch (e) {
-            console.log(e);
-            return null;
-        }
-    };
+  const fetchData = async () => {
+    const playerId = localStorage.getItem("playerId");
+    try {
+      //自分のapiサーバーにリクエストを送る
+      const res = await axios.get(`http://localhost:3001/players/${playerId}`);
+      const data = await res.data;
+      console.log(data);
+      return data;
+      //TODO 取得したデータをstateに保存
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+  };
 
-    useEffect(() => {
-        // fetch player by player id from backend
-        console.log("start");
-
-    }, []);
-  
   const getAPI = async () => {
-      const result = await fetchData();
-      if (result == null) {
-          setPlayers(dummy);
-      } else {
-          setPlayers(result);
-      }
-  }
+    const result = await fetchData();
+    if (result == null) {
+      setPlayers(dummy);
+    } else {
+      setPlayers(result);
+    }
+  };
 
   return (
     <div>
       <h2 style={{ textAlign: "center" }}>PlayerStatus</h2>
-        <button onClick={getAPI}>APIアクセス</button>
+      <button onClick={getAPI}>APIアクセス</button>
       <table>
         <thead>
           <tr>
